c97ad88c6faaf41dda84d85200a5e87c9459ee6c
evgeniy <uhvatkin.e@gigwork.ru>
2025-06-12 18:44:29 +0300

    тесты хекслет

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -26,6 +26,7 @@ class DifferTest extends TestCase
             'flat_json' => [file_get_contents(self::getFile('expected.txt')), genDiff(self::getFile('file1.json'), self::getFile('file2.json'))],
             'flat_yaml' => [file_get_contents(self::getFile('expected.txt')), genDiff(self::getFile('file1.yml'), self::getFile('file2.yml'))],
             'recursive_json' => [file_get_contents(self::getFile('expectedRecursive.txt')), genDiff(self::getFile('file1recursive.json'), self::getFile('file2recursive.json'))],
+            'recursive_yaml' => [file_get_contents(self::getFile('expectedRecursive.txt')), genDiff(self::getFile('file1recursive.yaml'), self::getFile('file2recursive.yaml'))],
             'plain_format' => [file_get_contents(self::getFile('expected2plain.txt')), genDiff(self::getFile('file1recursive.json'), self::getFile('file2recursive.json'), 'plain')],
             'json_format' => [file_get_contents(self::getFile('expectedJson.txt')), genDiff(self::getFile('file1recursive.json'), self::getFile('file2recursive.json'), 'json')],
 
