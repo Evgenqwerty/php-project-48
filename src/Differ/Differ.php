8c493a4e5cac216ae55ddf358c144fdedfec2ab4
evgeniy <uhvatkin.e@gigwork.ru>
2025-07-01 21:22:44 +0300

    линтер хекслета

--- src/Differ/Differ.php
+++ src/Differ/Differ.php
@@ -5,11 +5,6 @@ namespace Differ\Differ;
 use function Differ\Parser\parse;
 use function Differ\Formatters\render;
 
-/**
- * @param string $firstPath
- * @param string $secondPath
- * @param string $format
- */
 function genDiff(string $firstPath, string $secondPath, string $format = "stylish"): string|false
 {
     $firstArray = parse($firstPath);
@@ -25,14 +20,24 @@ function genDiff(string $firstPath, string $secondPath, string $format = "stylis
  */
 function makeDiff(object $before, object $after): array
 {
-    $unionKeys = array_unique(array_merge(
-        array_keys((array)$before),
-        array_keys((array)$after)
-    ));
+    $beforeKeys = array_keys((array)$before);
+    $afterKeys = array_keys((array)$after);
 
-    // Сортируем ключи без мутации исходного массива
-    $sortedKeys = array_values($unionKeys);
-    sort($sortedKeys);
+    $unionKeys = array_unique([...$beforeKeys, ...$afterKeys]);
+
+    // Сортировка без мутации - создаем новый отсортированный массив
+    $sortedKeys = array_reduce(
+        $unionKeys,
+        function ($carry, $item) {
+            $insertPos = 0;
+            while ($insertPos < count($carry) && strcmp($carry[$insertPos], $item) < 0) {
+                $insertPos++;
+            }
+            array_splice($carry, $insertPos, 0, $item);
+            return $carry;
+        },
+        []
+    );
 
     return array_map(function ($key) use ($before, $after) {
         if (!property_exists($before, $key)) {
@@ -51,22 +56,13 @@ function makeDiff(object $before, object $after): array
     }, $sortedKeys);
 }
 
-/**
- * @param string $typeNode
- * @param string $key
- * @param mixed $oldValue
- * @param mixed $newValue
- * @param array<int, array<string, mixed>>|null $children
- * @return array<string, mixed>
- */
 function buildNode(string $typeNode, string $key, mixed $oldValue, mixed $newValue, ?array $children = null): array
 {
-    $node = [
+    return [
         'typeNode' => $typeNode,
         'key' => $key,
         'oldValue' => $oldValue,
         'newValue' => $newValue,
         'children' => $children
     ];
-    return $node;
 }
